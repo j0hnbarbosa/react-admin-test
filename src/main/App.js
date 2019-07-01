d06ac3057fb0f2b5d19d267317b56bfcb680becf
John Barbosa <john@smtsolucoes.com.br>
2019-07-01 15:59:08 -0300

    fetch data

--- src/main/App.js
+++ src/main/App.js
@@ -10,8 +10,8 @@ import Dashboard from './Dashboard';
 import authProvider from './authProvider';
 
 
-// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-const dataProvider = jsonServerProvider('https://raw.githubusercontent.com/j0hnbarbosa/react-admin-test/master/src/data/data');
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+// const dataProvider = jsonServerProvider('https://raw.githubusercontent.com/j0hnbarbosa/react-admin-test/master/src/data/data');
 const App = () => (
     <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
